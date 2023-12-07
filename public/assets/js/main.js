d7e385eee0d68d76bdec4c267dc85a8ba567243b
Nagromn <morganwtt-dev@pm.me>
2023-12-07 17:56:28 +0100

    correction code, optimisation

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -3,12 +3,10 @@ import { Carousel } from "./components/Carousel.js";
 
 // Main function that initializes the application
 export const Main = () => {
-
   console.log("Hello from main.js");
 
   // Adding an event listener for when the DOM content is loaded
   document.addEventListener("DOMContentLoaded", () => {
-
     // Calling the Carousel function to set up the carousel functionality
     Carousel();
   });
